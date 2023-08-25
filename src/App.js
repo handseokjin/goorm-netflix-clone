8b8d703aec7b9a3553e675fc0d9021d6209700ff
JoowonKim <99549623+joowining@users.noreply.github.com>
2023-08-25 17:22:32 +0900

    extra merge with mainstream

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import './App.css';
-<<<<<<< HEAD
 import { RecoilRoot } from 'recoil';
 import Home from './component/Home/home';
 import LanguagePage from './components/language/LanguagePage';
-=======
 import Mainstream from './Mainstream/components/Mainstream';
->>>>>>> mainstream
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
       <RecoilRoot>
         <Home />
       </RecoilRoot>
       <LanguagePage/>
-=======
-      <Mainstream />
->>>>>>> mainstream
+      <Mainstream/>
     </div>
   );
 }
